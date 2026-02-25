3ab637b20c8cbd24152b92d47b133a26298db976
DariusJderu1 <jderudarius@gmail.com>
2026-02-25 20:38:16 +0200

    Change constructor to class

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-function Book(title, author, pages, read) {
+// function Book(title, author, pages, read) {
 
-    if (!new.target)
-        throw Error("You must use the 'new' operator to call the constructor");
+//     if (!new.target)
+//         throw Error("You must use the 'new' operator to call the constructor");
     
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.id = crypto.randomUUID();
-}
-Book.prototype.changeStatus = function() {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+//     this.id = crypto.randomUUID();
+// }
+// Book.prototype.changeStatus = function() {
 
-    this.read = !this.read;
+//     this.read = !this.read;
+// }
+
+class Book {
+
+    title;
+    author;
+    pages;
+    read;
+    id = crypto.randomUUID();
+
+    constructor(title, author, pages, read) {
+
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    changeStatus() {
+
+        this.read = !this.read;
+    }
 }
 
 function addBook(bookList, title, author, pages, read) {
